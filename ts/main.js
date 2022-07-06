2ea757ae26e1df130ca320e219a6c89198299d80
IrwinJuice <sevriukovmk@gmail.com>
2022-07-06 20:03:47 +0300

    [add] size

--- ts/main.js
+++ ts/main.js
@@ -1,14 +1,17 @@
 import { Field } from './Field.js';
-let small;
-let medium;
-let large;
 function bootstrap() {
+    let small;
+    let middle;
+    let large;
+    let ultra;
     small = document.getElementById('small');
-    medium = document.getElementById('medium');
+    middle = document.getElementById('middle');
     large = document.getElementById('large');
+    ultra = document.getElementById('ultra');
     small.addEventListener('click', () => playSmall());
-    medium.addEventListener('click', () => playSmall()); // TODO
-    large.addEventListener('click', () => playSmall()); // TODO
+    middle.addEventListener('click', () => playMiddle());
+    large.addEventListener('click', () => playLarge());
+    ultra.addEventListener('click', () => playUltra());
 }
 bootstrap();
 /**
@@ -17,7 +20,28 @@ bootstrap();
 function playSmall() {
     const rows = 9;
     const columns = 9;
-    const mines = 10;
+    const mines = 18;
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
+function playMiddle() {
+    const rows = 18;
+    const columns = 18;
+    const mines = 36;
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
+function playLarge() {
+    const rows = 36;
+    const columns = 36;
+    const mines = 130;
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
+function playUltra() {
+    const rows = 130;
+    const columns = 130;
+    const mines = 1600;
     const field = new Field(rows, columns, mines);
     field.generateCells().render();
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,18 +1,20 @@
 import {Field} from './Field.js';
 
-
-let small: HTMLButtonElement;
-let medium: HTMLButtonElement;
-let large: HTMLButtonElement;
-
 function bootstrap() {
+    let small: HTMLButtonElement;
+    let middle: HTMLButtonElement;
+    let large: HTMLButtonElement;
+    let ultra: HTMLButtonElement;
+
     small  = document.getElementById('small')! as HTMLButtonElement;
-    medium = document.getElementById('medium')! as HTMLButtonElement;
+    middle = document.getElementById('middle')! as HTMLButtonElement;
     large  = document.getElementById('large')! as HTMLButtonElement;
+    ultra  = document.getElementById('ultra')! as HTMLButtonElement;
 
     small.addEventListener('click', () => playSmall());
-    medium.addEventListener('click', () => playSmall()); // TODO
-    large.addEventListener('click', () => playSmall()); // TODO
+    middle.addEventListener('click', () => playMiddle());
+    large.addEventListener('click', () => playLarge());
+    ultra.addEventListener('click', () => playUltra());
 }
 
 bootstrap();
@@ -23,13 +25,35 @@ bootstrap();
 function playSmall() {
     const rows    = 9;
     const columns = 9;
-    const mines   = 10;
+    const mines   = 18;
 
     const field = new Field(rows, columns, mines);
     field.generateCells().render();
 }
 
+function playMiddle() {
+    const rows    = 18;
+    const columns = 18;
+    const mines   = 36;
 
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
 
+function playLarge() {
+    const rows    = 36;
+    const columns = 36;
+    const mines   = 130;
 
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
 
+function playUltra() {
+    const rows    = 130;
+    const columns = 130;
+    const mines   = 1600;
+
+    const field = new Field(rows, columns, mines);
+    field.generateCells().render();
+}
